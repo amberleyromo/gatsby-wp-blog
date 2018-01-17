5adcc27c7388ac440e77034198713c96ba215af5
Amberley Romo <amberleyjohanna@gmail.com>
2018-01-17 13:56:09 -0600

    style: add focus style, meta information

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
 
 import Header from '../components/Header'
 import './index.css'
@@ -11,6 +12,15 @@ class DefaultLayout extends React.Component {
   render() {
     return (
       <div>
+        <Helmet
+          title="Amberley's Blog"
+          meta={[
+            { name: 'description', content: 'Writings and collection from Amberley Romo' },
+            { name: 'keywords', content: 'blog, tech, code, development, knitting' },
+          ]}
+        >
+          <html lang="en" />
+        </Helmet>
         <Header />
         <main>{this.props.children()}</main>
       </div>
