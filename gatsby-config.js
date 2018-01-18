56aa4529789cc9c7cb1d1d7508e183fa84bc1a5d
Amberley Romo <amberleyjohanna@gmail.com>
2018-01-17 20:40:26 -0600

    chore: add service worker, manifest and favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,31 +1,55 @@
 module.exports = {
   siteMetadata: {
-    title: 'amberley dot blog',
-    subtitle: 'Gatsby + WP'
+    title: `amberley dot blog`,
+    subtitle: `Gatsby + WP`
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
     {
-      resolve: 'gatsby-source-wordpress',
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        baseUrl: 'amberley.me',
-        protocol: 'https',
+        name: "Amberley Dot Blog",
+        short_name: "AmberleyDotBlog",
+        start_url: "/",
+        background_color: "#f49985",
+        theme_color: "#fff7f3",
+        display: "minimal-ui",
+        icons: [
+          {
+            src: `/favicons/icon-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/icon-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        baseUrl: `amberley.me`,
+        protocol: `https`,
         hostingWPCOM: false,
         useACF: false
       },
       verboseOutput: true,
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    'gatsby-plugin-glamor',
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-glamor`,
     {
-      resolve: 'gatsby-plugin-typography',
+      resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: 'src/utils/typography.js',
+        pathToConfigModule: `src/utils/typography.js`,
       }
     },
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-112664995-1",
         anonymize: true,
