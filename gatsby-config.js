9d3ea8aeab15dd0e57021a139b1a4e69012b7ab7
Amberley Romo <amberleyjohanna@gmail.com>
2018-01-17 18:30:46 -0600

    chore: adding tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,14 @@ module.exports = {
       options: {
         pathToConfigModule: 'src/utils/typography.js',
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-112664995-1",
+        anonymize: true,
+      },
+    },
   ],
   
 };
