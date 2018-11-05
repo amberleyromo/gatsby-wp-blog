b56e96ba6802f475a82e71aaa839b44b9d6bd320
Amberley Romo <amberleyjohanna@gmail.com>
2018-11-04 19:22:33 -0600

    chore: sadness

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react";
+import Link from "gatsby-link";
 
 const Header = () => (
   <header>
@@ -7,12 +7,11 @@ const Header = () => (
       <Link to="/">Howdy. I'm Amberley.</Link>
     </h1>
     <h2>
-      <a href="https://github.com/amberleyromo">Front-end dev</a>,{' '}
-      <a href="https://www.instagram.com/cassrabbit/">bunny mama</a>,{' '}
-      <a href="https://www.ravelry.com/people/amberleyknits">knit nerd</a>.{' '}
+      <a href="https://github.com/amberleyromo">Front-end dev</a>,{" "}
+      <a href="https://www.ravelry.com/people/amberleyknits">yarn nerd</a>.{" "}
       <a href="https://twitter.com/amberleyjohanna">Sometimes-tweeter</a>.
     </h2>
   </header>
-)
+);
 
-export default Header
+export default Header;
